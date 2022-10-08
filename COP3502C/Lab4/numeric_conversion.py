ae7d40eaa9536e7f487b0e49bb317d561e6656b5
Dylan <dylan1120long@gmail.com>
2022-10-07 20:36:34 -0400

    actually finished selection 1

--- COP3502C/Lab4/numeric_conversion.py
+++ COP3502C/Lab4/numeric_conversion.py
@@ -21,32 +21,41 @@ def main():
         print('3. Convert binary to hexadecimal')
         print('4. Quit')
         selection = input('Please enter an option: ')
-        numeric_input = input('Please enter the numeric string to convert: ')
+        numeric_input = input('Please enter the numeric string to convert: ').lower()
         
         # remove prefix
         if numeric_input[1] in ('x', 'b'):
             numeric_input = numeric_input[2:]
         
         # convert to list
-        input_list = list(map(int, numeric_input))
+        input_list = list(map(str, numeric_input))
+        
+        # convert hex letters to numbers
+        formatted_list = []
+        for num in input_list:
+            try:
+                key = ['a', 'b', 'c', 'd', 'e', 'f']
+                formatted_list.append(key.index(num) + 10)
+            except:
+                formatted_list.append(num)
+        
+        formatted_list = list(map(int, formatted_list))
 
+       # call the correct function for conversion
         if selection.lower() in ('1', 'decode hexadecimal'):
-            print(hex_char_decode(input_list))
+            print(hex_string_decode(formatted_list))
         elif selection.lower() in ('2', 'decode binary'):
-            print(hex_string_decode(input_list))
+            print(binary_string_decode(formatted_list))
         elif selection.lower() in ('3', 'convert binary to hexadecimal'):
-            print(binary_string_decode(input_list))
+            print(binary_to_hex(formatted_list))
         elif selection.lower() in ('4', 'quit'):
             quit()
     
-    def hex_char_decode(digit):
+    def hex_string_decode(digit):
         result = 0
         for i in range(0, len(digit)):
             result += digit[i] * 16 ** (len(digit) - 1 - i)
         return result
-
-    def hex_string_decode(hex):
-        print('temp')
 
     def binary_string_decode(binary):
         print('temp')
