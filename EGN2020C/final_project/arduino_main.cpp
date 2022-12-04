88ba8a43e8f06f50f7c4e48d983b135d000a6257
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2022-12-04 15:24:38 -0500

    single button working

--- EGN2020C/final_project/arduino_main.cpp
+++ EGN2020C/final_project/arduino_main.cpp
@@ -2,24 +2,27 @@
 
 // consider changing these ints to bytes to save memory and / or moving to a .h file
 // define inputs
-int nail1 = 2;
-int nail2 = 3;
-int nail3 = 4;
+int nail1 = 22;
+int nail2 = 24;
+int nail3 = 26;
 
-int screw1 = 5;
-int screw2 = 6;
-int screw3 = 7;
+char screw1[] = "A8";
+char screw2[] = "A9";
+int screw3 = 26;
 
 int option_button = 8;
 
 // define outputs
-int rgb_led1_red;
-int rgb_led1_green;
-int rgb_led1_blue;
-int led2 = 12;
-int led3 = 13;
-int led4;
-int led5;
+int rgb_led1_red = 2;
+int rgb_led1_green = 3;
+int rgb_led1_blue = 4;
+int rgb_led2_red = 5;
+int rgb_led2_green = 6;
+int rgb_led2_blue = 7;
+
+int led3 = 28;
+int led4 = 30;
+int led5 = 32;
 int led6;
 
 // set display pins here
@@ -44,9 +47,9 @@ const int SHORT_PRESS_TIME = 500; // 500 milliseconds
 // set 
 
 void setup() {
-    pinMode(nail1, INPUT_PULLUP);
-    pinMode(nail2, INPUT_PULLUP);
-    pinMode(nail3, INPUT_PULLUP);
+    pinMode(nail1, INPUT);
+    pinMode(nail2, INPUT);
+    pinMode(nail3, INPUT);
 
     pinMode(screw1, INPUT);
     pinMode(screw2, INPUT);
@@ -55,7 +58,9 @@ void setup() {
     pinMode(rgb_led1_red, OUTPUT);
     pinMode(rgb_led1_green, OUTPUT);
     pinMode(rgb_led1_blue, OUTPUT);
-    pinMode(led2, OUTPUT);
+    pinMode(rgb_led2_red, OUTPUT);
+    pinMode(rgb_led2_green, OUTPUT);
+    pinMode(rgb_led2_blue, OUTPUT);
     pinMode(led3, OUTPUT);
     pinMode(led4, OUTPUT);
     pinMode(led5, OUTPUT);
@@ -69,47 +74,68 @@ void setup() {
 
 
 void loop() {
+    // Serial.println(digitalRead(nail1));
+    Serial.println(analogRead(screw1));
+    
     option_button_current_state = digitalRead(option_button);
+    long pressed_time;
+    long released_time;
+    long press_duration;
 
-    // handle option button press (short and long presses)
-    if (option_button_last_state == HIGH && option_button_current_state == LOW) { // button was just pressed
-        long pressed_time = millis();
-    } else if (option_button_last_state == LOW && option_button_current_state == HIGH) { // button was just released
-        long released_time = millis();
 
-        long press_duration = released_time - pressed_time;
-        
-        if (press_duration < SHORT_PRESS_TIME) { // short press
-            // switch modes
-            if (!game_in_progress) {
-                if (current_mode == 2) {
-                    current_mode = 0;
-                } else {
-                    current_mode++;
-                }
-            } else {
-                // TODO: display error message 'can't change modes while game in progress!'
-            }
-        } else { // long press
-            // toggle advanced mode
-            advanced_mode_active = !advanced_mode_active;
-            // TODO: display message 'advanced mode on' or 'advanced mode off'
-        }
+    // temporary code to test button press
+    if (digitalRead(nail1) == HIGH) {
+        analogWrite(rgb_led1_red, 155);
+        analogWrite(rgb_led1_green, 67);
+        analogWrite(rgb_led1_blue, 243);
+    } else {
+        analogWrite(rgb_led1_red, 0);
+        analogWrite(rgb_led1_green, 0);
+        analogWrite(rgb_led1_blue, 0);
     }
 
-    // activate current mode
-        switch (current_mode) {
-            case 0:
-                // free play mode
-                free_play();
-                break;
-            case 1:
-                // pattern game mode
-                break;
-            case 2:
-                // timed game mode
-                break;
-        }
+
+
+
+    // handle option button press (short and long presses)
+    // if (option_button_last_state == HIGH && option_button_current_state == LOW) { // button was just pressed
+    //     pressed_time = millis();
+    // } else if (option_button_last_state == LOW && option_button_current_state == HIGH) { // button was just released
+    //     released_time = millis();
+
+    //     press_duration = released_time - pressed_time;
+        
+    //     if (press_duration < SHORT_PRESS_TIME) { // short press
+    //         // switch modes
+    //         if (!game_in_progress) {
+    //             if (current_mode == 2) {
+    //                 current_mode = 0;
+    //             } else {
+    //                 current_mode++;
+    //             }
+    //         } else {
+    //             // TODO: display error message 'can't change modes while game in progress!'
+    //         }
+    //     } else { // long press
+    //         // toggle advanced mode
+    //         advanced_mode_active = !advanced_mode_active;
+    //         // TODO: display message 'advanced mode on' or 'advanced mode off'
+    //     }
+    // }
+
+    // // activate current mode
+    //     switch (current_mode) {
+    //         case 0:
+    //             // free play mode
+    //             free_play();
+    //             break;
+    //         case 1:
+    //             // pattern game mode
+    //             break;
+    //         case 2:
+    //             // timed game mode
+    //             break;
+    //     }
 
 }
 
@@ -123,24 +149,32 @@ int free_play() {
     rgb_value_separator(rgb1_value, arr);
     
     analogWrite(rgb_led1_red, arr[0]);
-    analog_Write(rgb_led1_green, arr[1]);
-    analog_Write(rgb_led1_blue, arr[2]);
+    analogWrite(rgb_led1_green, arr[1]);
+    analogWrite(rgb_led1_blue, arr[2]);
      
     
     
     // turn on led when nail is hit
     if (digitalRead(nail1) == HIGH) {
-        digitalWrite(led1, HIGH);
+        analogWrite(rgb_led1_red, 255);
+        analogWrite(rgb_led1_green, 0);
+        analogWrite(rgb_led1_blue, 0);
         lifetime_nails_hit++;
     } else {
-        digitalWrite(led1, LOW);
+        analogWrite(rgb_led1_red, 0);
+        analogWrite(rgb_led1_green, 0);
+        analogWrite(rgb_led1_blue, 0);
     }
 
     if (digitalRead(nail2) == HIGH) {
-        digitalWrite(led2, HIGH);
+        analogWrite(rgb_led2_red, 255);
+        analogWrite(rgb_led2_green, 0);
+        analogWrite(rgb_led2_blue, 0);
         lifetime_nails_hit++;
     } else {
-        digitalWrite(led2, LOW);
+        analogWrite(rgb_led2_red, 0);
+        analogWrite(rgb_led2_green, 0);
+        analogWrite(rgb_led2_blue, 0);
     }
 
     if (digitalRead(nail3) == HIGH) {
