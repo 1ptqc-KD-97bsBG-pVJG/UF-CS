ba2ad9a5a7e078939e926499996eadac6bce534b
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2023-01-19 22:47:13 -0500

    zybooks happy

--- COP3503C/Lab1/main.cpp
+++ COP3503C/Lab1/main.cpp
@@ -14,17 +14,17 @@ int main()
     vector<int> column2;
     // Overall flow of the program:
     // Get some input for the title
-    cout << "Enter a title for the data: " << endl;
+    cout << "Enter a title for the data:" << endl;
     getline(cin, title);
     cout << "You entered: " << title << endl;
 
     // Get some input for column header #1
-    cout << "Enter the column 1 header: " << endl;
+    cout << "Enter the column 1 header:" << endl;
     getline(cin, column1Title);
     cout << "You entered: " << column1Title << endl;
 
     // Get some input for column header #2
-    cout << "Enter the column 2 header: " << endl;
+    cout << "Enter the column 2 header:" << endl;
     getline(cin, column2Title);
     cout << "You entered: " << column2Title << endl;
    
@@ -32,7 +32,7 @@ int main()
     // Until the user types "-1":
     while (userDataInput != "-1") {
     // Get input for a string
-    cout << "Enter a data point (-1 to stop input): " << endl;
+    cout << "Enter a data point (-1 to stop input):" << endl;
     getline(cin, userDataInput);
     // Validate input
     // If valid...
@@ -40,7 +40,7 @@ int main()
             // comma found, good
             // check if there are multiple commas
             if (userDataInput.find(",", userDataInput.find(",") + 1) != string::npos) { 
-                cout << "Too many commas in input." << endl;
+                cout << "Error: Too many commas in input." << endl;
             } else {
                 // only one comma, now split string on comma and space
                 string first = userDataInput.substr(0, userDataInput.find(","));
@@ -60,27 +60,30 @@ int main()
                 // second is not a number
                 catch(const std::exception& e)
                 {
-                    cout << "Comma not followed by an integer." << endl;
+                    cout << "Error: Comma not followed by an integer." << endl;
                 }
             }
         } else {
             // no comma found, bad
             if (userDataInput != "-1") {
-                cout << "No comma in string." << endl;
+                cout << "Error: No comma in string." << endl;
             }
         }
    }
 
     // Print out data in table format
+    cout << endl;
     cout << setw(33) << right << title << endl;
-    cout << setw(20) << left << column1Title << " | " << setw(23) << right << column2Title << endl;
+    cout << setw(20) << left << column1Title << "|" << setw(23) << right << column2Title << endl;
     cout << "--------------------------------------------" << endl;
-    for (int i = 0; i < column1.size() - 1; i++ ) {
-        cout << setw(20) << left << column1[i] << " | " << setw(23) << right << column2[i] << endl;
+    for (int i = 0; i < column1.size(); i++ ) {
+        cout << setw(20) << left << column1.at(i) << "|" << setw(23) << right << column2.at(i) << endl;
     }
     
+    cout << endl;
+
     // Print out data in chart format
-    for (int i = 0; i < column1.size() - 1; i++ ) {
+    for (int i = 0; i < column1.size(); i++ ) {
         string dotsDisplay;
         for (int j = 0; j < column2[i]; j++) {
             dotsDisplay += "*";
