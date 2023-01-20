bd8fe35434ff7f36a812925ef0bb96b7b1227f12
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2023-01-19 21:16:54 -0500

    95% done

--- COP3503C/Lab1/main.cpp
+++ COP3503C/Lab1/main.cpp
@@ -36,21 +36,57 @@ int main()
     getline(cin, userDataInput);
     // Validate input
     // If valid...
-        if () {
+        if (userDataInput.find(",") != string::npos) {
+            // comma found, good
+            // check if there are multiple commas
+            if (userDataInput.find(",", userDataInput.find(",") + 1) != string::npos) { 
+                cout << "Too many commas in input." << endl;
+            } else {
+                // only one comma, now split string on comma and space
+                string first = userDataInput.substr(0, userDataInput.find(","));
+                string second = userDataInput.substr(userDataInput.find(",") + 2);
 
+                // check if second is a number
+                try
+                {
+                    int second_num = stoi(second);
+                    // add to vectors
+                    column1.push_back(first);
+                    column2.push_back(second_num);
+
+                    cout << "Data string: " << first << endl;
+                    cout << "Data integer: " << second_num << endl;
+                }
+                // second is not a number
+                catch(const std::exception& e)
+                {
+                    cout << "Comma not followed by an integer." << endl;
+                }
+            }
+        } else {
+            // no comma found, bad
+            if (userDataInput != "-1") {
+                cout << "No comma in string." << endl;
+            }
         }
-        // Split
-        // Validate second half of input
-        // If also valid...
-             // Store separate pieces
-    
-
-
    }
 
-//    Print out data in table format
-   
-//    Print out data in chart format
+    // Print out data in table format
+    cout << setw(33) << right << title << endl;
+    cout << setw(20) << left << column1Title << " | " << setw(23) << right << column2Title << endl;
+    cout << "--------------------------------------------" << endl;
+    for (int i = 0; i < column1.size() - 1; i++ ) {
+        cout << setw(20) << left << column1[i] << " | " << setw(23) << right << column2[i] << endl;
+    }
+    
+    // Print out data in chart format
+    for (int i = 0; i < column1.size() - 1; i++ ) {
+        string dotsDisplay;
+        for (int j = 0; j < column2[i]; j++) {
+            dotsDisplay += "*";
+        }
+        cout << setw(20) << right << column1[i] << " " << dotsDisplay << endl;
+    }
    
    return 0;
 }
