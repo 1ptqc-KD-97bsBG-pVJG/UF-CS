c4f11995200e877e87cc62b1241e5feebf0f5144
Dylan <dylan1120long@gmail.com>
2023-03-29 21:34:12 -0400

    working on image actions

--- COP3503C/Project2/src/ImageFunctions.cpp
+++ COP3503C/Project2/src/ImageFunctions.cpp
@@ -1,3 +1,25 @@
 #include "ImageFunctions.h"
 #include "Image.h"
 using namespace std;
+
+unsigned int clamp (int preClampValue) {
+  if (preClampValue < 0) {
+    return 0;
+  } else if (preClampValue > 255) {
+    return 255;
+  } else {
+    return preClampValue;
+  }
+}
+
+
+// image actions
+
+Image Multiply(Image &firstImage, Image &secondImage) {
+  Image imageResult;
+
+  Image::Header header = firstImage.getHeader();
+  imageResult.setHeader(header);
+  
+
+}
--- COP3503C/Project2/src/ImageFunctions.h
+++ COP3503C/Project2/src/ImageFunctions.h
@@ -2,9 +2,29 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include "Image.h"
 using namespace std;
 
+float scale(float scale);
 
+unsigned int clamp(int preClampValue);
 
+// image actions
+Image Multiply(Image &firstImage, Image &secondImage);
 
+Image Subtract(Image &firstImage, Image &secondImage);
+
+Image Screen(Image &firstImage, Image &secondImage);
+
+Image Overlay(Image &firstImage, Image &secondImage);
+
+Image addGreen(Image &image);
+
+Image scaleRed(Image &image);
+
+Image splitChannels(string rgbValue, Image &image);
+
+Image Combine(Image &firstImage, Image &secondImage, Image &thirdImage);
+
+Image Rotate(Image &image);
