cfefb7b9791397bcbd1f26e7d7a05cbd6390cfc6
Dylan <dylan1120long@gmail.com>
2023-03-31 17:39:14 -0400

    merge fix

--- COP3503C/Project2/src/main.cpp
+++ COP3503C/Project2/src/main.cpp
@@ -15,9 +15,6 @@ void printHelp() {
     std::cout << "  ./project2.out [output] [firstImage] [method] [...]" << std::endl;
 }
 
-bool isNumber(const std::string& s) {
-    return !s.empty() && std::find_if(s.begin(), s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
-}
 
 int main(int argc, char* argv[]) {
     cout << "argc: " << argc << endl;
