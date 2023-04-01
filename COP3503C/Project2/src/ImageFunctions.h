949b32f7cb16da9dcd3e7011dd56d165fbcecbfc
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2023-03-31 22:08:57 -0400

    fix for add red build error

--- COP3503C/Project2/src/ImageFunctions.h
+++ COP3503C/Project2/src/ImageFunctions.h
@@ -19,11 +19,11 @@ Image Screen(Image& firstImage, Image& secondImage);
 
 Image Overlay(Image& firstImage, Image& secondImage);
 
-Image addRed(Image& image, unsigned int amount = 200);
+Image addRed(Image& image, int amount = 200);
 
-Image addGreen(Image& image, unsigned int amount = 200);
+Image addGreen(Image& image, int amount = 200);
 
-Image addBlue(Image& image, unsigned int amount = 200);
+Image addBlue(Image& image, int amount = 200);
 
 Image scaleRed(Image& image, float amount = 4.0f);
 
