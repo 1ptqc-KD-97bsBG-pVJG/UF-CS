fbb5284195208b9ed16f4c4813823e51640cf513
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2023-04-18 22:27:04 -0400

    added inital version of leaderboard

--- COP3503C/Project3/main.cpp
+++ COP3503C/Project3/main.cpp
@@ -2,7 +2,103 @@
 #include <cctype>
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
+
+
+int displayLeaderboard() {
+    //    open leaderboard file to load times and names in the format: 07:41,Alex
+
+    sf::Event event;
+
+
+
+    ifstream leaderboardFile("../files/leaderboard.txt");
+    if (!leaderboardFile) {
+        cerr << "Error opening leaderboard file!" << endl;
+        return EXIT_FAILURE;
+    }
+    string line;
+    vector<string> leaderboard;
+    int lineIterator = 1;
+    while (getline(leaderboardFile, line)) {
+        string formattedLine;
+        int postition = line.find(",");
+        formattedLine = to_string(lineIterator) + ".\t" + line.substr(0, postition) + "\t" + line.substr(postition + 1, line.length());
+
+//        You will also display the contents of the "leaderboard.txt" file with the following specifications:
+//        – Style: Bold
+//        – Color: White (Default)
+//        – Size: 18px
+//        – Position: width/2.0f, height/2.0f + 20
+//        – Note: It will be easier for you if you store all the rows in a string separated by two line spaces (\n\n). Then you can use this string to create a sf::Text and set its position as specified above. Also, you can give a tab space (\t) between the values
+//        i.e., ("1." + "\t" + "07:01" + "\t" + "Alex").
+
+
+
+        leaderboard.push_back(formattedLine);
+        lineIterator++;
+    }
+    leaderboardFile.close();
+
+
+
+    std::cout << "Iterated through leaderboard file" << std::endl;
+
+
+
+
+
+    sf::RenderWindow leaderboardWindow(sf::VideoMode(800, 600), "Minesweeper");
+    // Load the font
+    sf::Font font;
+    if (!font.loadFromFile("../files/font.ttf")) {
+        std::cerr << "Error opening font file!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Set up leaderboard text
+    sf::Text leaderboardTitle("LEADERBOARD", font, 20);
+    leaderboardTitle.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    leaderboardTitle.setFillColor(sf::Color::White);
+    sf::FloatRect leaderboardTitleBounds = leaderboardTitle.getLocalBounds();
+    leaderboardTitle.setOrigin(leaderboardTitleBounds.left + leaderboardTitleBounds.width / 2.0f,
+                               leaderboardTitleBounds.top + leaderboardTitleBounds.height / 2.0f);
+    leaderboardTitle.setPosition(leaderboardWindow.getSize().x / 2.0f, leaderboardWindow.getSize().y / 2.0f - 200);
+
+    leaderboardWindow.clear(sf::Color::Blue);
+    leaderboardWindow.draw(leaderboardTitle);
+
+    std::cout << "iterating..." << std::endl;
+    for (int i = 0; i < leaderboard.size(); i++) {
+        // display leaderboard text on screen
+        std::cout << leaderboard[i] << std::endl;
+        sf::Text leaderboardText(leaderboard[i], font, 18);
+        leaderboardText.setFillColor(sf::Color::White);
+        sf::FloatRect leaderboardTextBounds = leaderboardText.getLocalBounds();
+        leaderboardText.setOrigin(leaderboardTextBounds.left + leaderboardTextBounds.width / 2.0f,
+                              leaderboardTextBounds.top + leaderboardTextBounds.height / 2.0f);
+        leaderboardText.setPosition(leaderboardWindow.getSize().x / 2.0f, leaderboardWindow.getSize().y / 2.0f - 100 + (i * 50)); // FIXME: not sure how much spacing is required between scores
+        leaderboardWindow.draw(leaderboardText);
+    }
+
+
+    leaderboardWindow.display();
+
+    while (leaderboardWindow.isOpen()) {
+        // Gameplay code here
+        while (leaderboardWindow.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                leaderboardWindow.close();
+            }
+        }
+    }
+
+
+
+
+    return 0;
+}
 
 int main()
 {
@@ -124,14 +220,8 @@ int main()
         return EXIT_FAILURE;
     }
     else {
-        sf::RenderWindow gameWindow(sf::VideoMode(800, 600), "Minesweeper");
-        while (gameWindow.isOpen()) {
-            // Gameplay code here
-            while (gameWindow.pollEvent(event)) {
-                if (event.type == sf::Event::Closed) {
-                    gameWindow.close();
-                }
-            }
-        }
+        std::cout << "Calling function" << std::endl;
+        displayLeaderboard();
+
     }
 }
