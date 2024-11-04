8a20f21773f8a38c4f6c835d02db4c991b680b89
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2024-11-03 20:23:18 -0500

    allocate function v1

--- COP4600/Project 2/MemoryManager.cpp
+++ COP4600/Project 2/MemoryManager.cpp
@@ -1,5 +1,7 @@
 #include "MemoryManager.h"
 
+using namespace std;
+
   // allocator functions, do not belong to MemoryManager class:
 
   // Returns word offset of hole selected by the best fit memory allocation algorithm, and -1 if there is no fit.
@@ -21,8 +23,49 @@
 
 
 // allocate:
-void* MemoryManager::allocate(size_t, sizeInBytes){
-  // errenesto discusses more at 25:00
+void* MemoryManager::allocate(size_t sizeInBytes){
+  if (memoryStart == nullptr || sizeInBytes == 0) {
+    return nullptr;
+  }
+
+  size_t sizeInWords = (sizeInBytes + wordSize - 1) / wordSize;
+  uint16_t* holeList = static_cast<uint16_t*>(getList());
+  
+  if (holeList == nullptr) {
+    return nullptr;
+  }
+
+  int offset = allocatorFunction(static_cast<int>(sizeInWords), holeList);
+  delete[] holeList;
+
+  if (offset == -1) {
+    return nullptr;
+  }
+
+  size_t offsetWords = static_cast<size_t>(offset);
+  auto it = find_if(blocks.begin(), blocks.end(), [offsetWords](const Block& block) {
+    return block.offset == offsetWords && block.isFree;
+  });
+
+  if (it == blocks.end()) {
+    return nullptr;
+  }
+
+  Block& hole = *it;
+  if (hole.length < sizeInWords) {
+    return nullptr;
+  }
+
+  if (hole.length == sizeInWords) {
+    hole.isFree = false;
+  } else {
+    Block allocatedBlock = {hole.offset, sizeInWords, false};
+    hole.offset += sizeInWords;
+    hole.length -= sizeInWords;
+    blocks.insert(it, allocatedBlock);
+  }
+
+  return static_cast<void*>(static_cast<uint8_t*>(memoryStart) + offsetWords * wordSize);
 }
 
 // free:
